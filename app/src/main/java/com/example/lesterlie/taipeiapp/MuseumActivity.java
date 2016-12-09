d4853e0cd303c59cfe94c4dc5e5fe4f27ebf1ebd
waakye <dailyomnivore@gmail.com>
2016-12-10 04:20:37 +0800

    Add new activities for the five types of attractions

---
+++ app/src/main/java/com/example/lesterlie/taipeiapp/MuseumActivity.java
@@ -0,0 +1,13 @@
+package com.example.lesterlie.taipeiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MuseumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_museum);
+    }
+}
