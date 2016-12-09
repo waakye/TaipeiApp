1ce58fd77c54def879292e5360ec03995037c40b
waakye <dailyomnivore@gmail.com>
2016-12-10 05:04:31 +0800

    Add AttractionClickListener class and use it to create an OnClickListener for Museum category

--- app/src/main/java/com/example/lesterlie/taipeiapp/MainActivity.java
+++ app/src/main/java/com/example/lesterlie/taipeiapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lesterlie.taipeiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AttractionClickListener clickListener = new AttractionClickListener();
+
+        // Find the View that shows the museums category
+        TextView museums = (TextView)findViewById(R.id.museums);
+
+        museums.setOnClickListener(clickListener);
     }
 }
