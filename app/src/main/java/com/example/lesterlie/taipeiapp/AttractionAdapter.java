29eea4a376a9e3b1afb57f8aadefc73a9b3bf374
waakye <dailyomnivore@gmail.com>
2016-12-14 20:16:23 +0800

    Add Custom ArrayAdapter and update list_item.xml and populate data for museums

---
+++ app/src/main/java/com/example/lesterlie/taipeiapp/AttractionAdapter.java
@@ -0,0 +1,113 @@
+package com.example.lesterlie.taipeiapp;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * AttractionAdapter 
+ * Created by lesterlie on 12/14/16.
+ */
+
+public class AttractionAdapter extends ArrayAdapter<Attraction> {
+
+    /**
+     * This is Udacity's custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we ant to
+     * populate into the lists
+     */
+    public AttractionAdapter(Activity context, ArrayList<Attraction> attractions) {
+        // Here we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView
+        // Because this is a custom adapter for two TextViews, the adapter is not going to use
+        // this second argument , so it can be any value.  Here we used 0.
+        super(context, 0, attractions);
+    }
+
+    /**
+     * Provide a view for an AdapterView (ListView, GridView, etc.)
+     * @param position The position in the list of data that should be displayed in the list item
+     *                 view.
+     * @param convertView The recycled view to populate.
+     * @param parent    The parent ViewGroup that is used for inflation
+     * @return          The view for the position in the AdapterView
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Attraction} object located at this position in the list
+        Attraction currentAttraction = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID attractionEnglishName
+        TextView englishNameTextView =
+                (TextView)listItemView.findViewById(R.id.attraction_english_name);
+        // Get the attraction English name from the current attraction object and set this text on
+        // the englishNameTextView
+        englishNameTextView.setText(currentAttraction.getEnglishName());
+
+        // Find the TextView in the list_item.xml layout with the ID attractionPinyinName
+        TextView pinyinNameTextView =
+                (TextView)listItemView.findViewById(R.id.attraction_pinyin_name);
+        // Get the attraction Pinyin name from the current attraction object and set this text on
+        // the pinyinNameTextView
+        pinyinNameTextView.setText(currentAttraction.getPinyinName());
+
+        // Find the TextView in the list_item.xml layout with the ID admissionFees
+        TextView admissionFeesTextView =
+                (TextView)listItemView.findViewById(R.id.attraction_admission_fees);
+        // Get the attraction admission fees from the current attraction object and set this text on
+        // the admissionFeesTextView
+        admissionFeesTextView.setText(currentAttraction.getAdmissionFees());
+
+        // Find the TextView in the list_item.xml layout with the ID operatingHours
+        TextView operatingHoursTextView =
+                (TextView)listItemView.findViewById(R.id.attraction_operating_hours);
+        // Get the attraction operating hours from the current attraction object and set this text
+        // on the operatingHoursTextView
+        operatingHoursTextView.setText(currentAttraction.getOperatingHours());
+
+        // Find the TextView in the list_item.xml layout with the ID attractionWebsite
+        TextView attractionWebsiteTextView =
+                (TextView)listItemView.findViewById(R.id.attraction_website);
+        // Get the attraction operating hours from the current attraction object and set this text
+        // on the attractionWebsiteTextView
+        attractionWebsiteTextView.setText(currentAttraction.getAttractionWebsite());
+
+        // Find the TextView in the list_item.xml layout with the ID attractionEnglishAddress
+        TextView englishAddressTextView =
+                (TextView)listItemView.findViewById(R.id.attraction_english_address);
+        // Get the attraction English address from the current attraction object and set this text
+        // on the englishAddressTextView
+        englishAddressTextView.setText(currentAttraction.getEnglishAddress());
+
+        // Find the TextView in the list_item.xml layout with the ID attractionPinyinName
+        TextView pinyinAddressTextView =
+                (TextView)listItemView.findViewById(R.id.attraction_pinyin_address);
+        // Get the attraction Pinyin address from the current attraction object and set this text on
+        // the pinyinAddressTextView
+        pinyinAddressTextView.setText(currentAttraction.getPinyinAddress());
+
+        // Find the TextView in the list_item.xml layout with the ID attractionEnglishDescription
+        TextView descriptionTextView =
+                (TextView) listItemView.findViewById(R.id.attraction_english_description);
+        // Get the attraction English description from teh current attraction object and set this
+        // text on the description TextView
+        descriptionTextView.setText(currentAttraction.getEnglishDescription());
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView
+        return listItemView;
+    }
+}
--- app/src/main/java/com/example/lesterlie/taipeiapp/MuseumActivity.java
+++ app/src/main/java/com/example/lesterlie/taipeiapp/MuseumActivity.java
@@ -17,21 +17,75 @@ public class MuseumActivity extends AppCompatActivity {
         setContentView(R.layout.activity_attraction);
 
         // Create an arrayList of museums
-        ArrayList<String> museums = new ArrayList<String>();
-        museums.add("National Palace Museum");
-        museums.add("Taipei Fine Arts Museum");
-        museums.add("National Museum of History");
-        museums.add("National Taiwan Museum");
-        museums.add("Museum of Contemporary Art");
-        museums.add("Shung Ye Museum of Formosan Aborigines");
+        ArrayList<Attraction> attractions = new ArrayList<Attraction>();
+        attractions.add(new Attraction(
+                "National Palace Museum",
+                "Gugong Bowuguan",
+                "Regular admission: NT250",
+                "Open daily from 08:30 to 18:30 all year round.\n" +
+                        "Extension of opening hours at night: 18:30 - 21:00 on Fridays and Saturdays.",
+                "www.npm.gov.tw",
+                "No. 221, Section 2, Zhishan Rd, Shilin District",
+                "Shilin Qu, Zhishan Lu, Er Duan, Liang Bai Er Shi Yi Hao",
+                "A permanent collection of nearly 700,000 pieces of ancient Chinese imperial " +
+                        "artifacts and artworks, making it one of the largest of its type in the " +
+                        "world. "));
+        attractions.add(new Attraction(
+                "Taipei Fine Arts Museum",
+                "Taibei Shili Meishuguan",
+                "Regular admission: NT30",
+                "Tuesdays through Friday and Sunday open 9:30 to 17:30; " +
+                        "open until 20:30 on Saturdays; closed on Mondays",
+                "http://www.tfam.museum/",
+                "No. 181, Section 3, Zhongshan N Rd, Zhongshan District",
+                "Zhongshan Qu, Zhongshan Bei Lu, San Duan, Yibai BaShi Yi Hao",
+                "The first museum in Taiwan built for contemporary art exhibitions. "));
+        attractions.add(new Attraction(
+                "National Museum of History",
+                "Lishi Bowuguan",
+                "Regular admission: NT30",
+                "Tuesdays through Sunday open 10:00 to 18:00",
+                "http://www.nmh.gov.tw/",
+                "No. 49, Nanhai Rd, Zhongzheng District",
+                "Zhongzheng qu, Nanhai Lu, Sishi Jiu Hao",
+                "Exhibits Taiwanese historical items."));
+        attractions.add(new Attraction(
+                "National Taiwan Museum",
+                "Guoli Taiwan Bowuguan",
+                "Regular admission: NT30",
+                "Tuesdays through Sunday open 09:30 to 17:00",
+                "www2.ntm.gov.tw",
+                "No. 2, Xiangyang Rd, Zhongzheng District",
+                "Zhongzheng Qu, Xiangyang Lu, Er Hao",
+                "The collection and research continue to focus on anthropology, earth sciences, " +
+                        "zoology, and botany that are relevant to Taiwan. "));
+        attractions.add(new Attraction(
+                "Museum of Contemporary Art",
+                "Taibei Dangdai Yishuguan",
+                "Regular admission: NT50",
+                "Tuesdays through Sunday open 10:00 to 18:00",
+                "http://www.mocataipei.org.tw/",
+                "No.39, Chang'an W. Rd., Datong District",
+                "Datong Qu, Chang An Xilu, Sanshi Jiu Hao",
+                "A museum displaying contemporary Taiwan art."));
+        attractions.add(new Attraction(
+                "Shung Ye Museum of Formosan Aborigines",
+                "Shunyi Yaiwan Yuan Zhumin Bowuguan",
+                "Regular admission: NT150",
+                "Tuesdays through Sunday open 09:00 to 17:00",
+                "http://www.museum.org.tw/symm_en/index.htm",
+                "No. 282, Sec.2 Zhishan Rd., Shihlin District",
+                "Shilin Qu, Zhishan Lu, Er Duan, Liang Bai Bashi Er Hao",
+                "A museum dedicated to displaying the cultures and histories of the " +
+                        "Taiwanese aborigines. "));
 
         // Create an {@link ArrayAdapter}, whose data source is a activity_attraction of Strings. The adapter
         // knows how to create layouts for each item in the activity_attraction, using the simple_list_item_1.xml
         // layout resource defined in the Android framework.
         // This activity_attraction item layout contains a single {@link TextView}, which the adapter will set
         // to display a single word.
-        ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<String>(this, R.layout.list_item, museums);
+        AttractionAdapter adapter =
+                new AttractionAdapter(this, attractions);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.  There
         // should be a {@link ListView} with the view ID called activity_attraction, which is declared in the
@@ -42,6 +96,6 @@ public class MuseumActivity extends AppCompatActivity {
         // {@link ListView} will display activity_attraction items for each word in the activity_attraction of attractions.
         // Do this by calling the setAdapter method on the {@link ListView} object and pass in 1
         // argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        listView.setAdapter(itemsAdapter);
+        listView.setAdapter(adapter);
     }
 }
